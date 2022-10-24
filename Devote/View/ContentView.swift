c291e94facd00ac46fd8058274e5e3c4c064b004
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-24 12:45:34 +0000

    add sound, haptics and feedbacks

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -67,6 +67,8 @@ struct ContentView: View {
                         
                         Button {
                             isDarkMode.toggle()
+                            playSound(sound: "sound-tap", type: "mp3")
+                            feedback.notificationOccurred(.success)
                         } label: {
                             Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
                                 .resizable()
@@ -81,7 +83,10 @@ struct ContentView: View {
                     
                     //MARK: - New Task Button
                     
-                    Button(action: { showNewTaskItem = true }) {
+                    Button(action: { showNewTaskItem = true
+                        playSound(sound: "sound-ding", type: "mp3")
+                        feedback.notificationOccurred(.success)
+                    }) {
                         Image(systemName: "plus.circle")
                             .font(.system(size: 30, weight: .semibold, design: .rounded))
                         Text("New Task")
